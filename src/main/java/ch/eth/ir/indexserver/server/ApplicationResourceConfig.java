1b2db617daf1626eda0b42c701ae9c8e4a5906cd
Bernhard Kratzwald <bernhard2202@gmail.com>
2016-08-29 18:00:14 +0200

    refactoring

--- src/main/java/ch/eth/ir/indexserver/server/ApplicationResourceConfig.java
+++ src/main/java/ch/eth/ir/indexserver/server/ApplicationResourceConfig.java
@@ -3,18 +3,20 @@ package ch.eth.ir.indexserver.server;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.server.ServerProperties;
 
-import ch.eth.ir.indexserver.server.exception.IOExceptionMapper;
+import ch.eth.ir.indexserver.server.exception.CustomExceptionMapper;
+import ch.eth.ir.indexserver.server.security.AuthenticationFilter;
 import ch.eth.ir.indexserver.server.utilities.IndexReaderBinder;
 
 public class ApplicationResourceConfig extends ResourceConfig {
 
 	public ApplicationResourceConfig() {
-		packages("ch.eth.ir.indexserver.resource");
+		packages("ch.eth.ir.indexserver.server.resource");
 		// Set this property so that the 400 will still send the entity
 		// correctly.
 		property(ServerProperties.RESPONSE_SET_STATUS_OVER_SEND_ERROR, "true");
 		// register for singleton injection
         register(new IndexReaderBinder());
-        register(new IOExceptionMapper());
+        register(new CustomExceptionMapper());
+        register(AuthenticationFilter.class);
 	}
 }
--- src/main/java/ch/eth/ir/indexserver/server/exception/CustomExceptionMapper.java
+++ src/main/java/ch/eth/ir/indexserver/server/exception/CustomExceptionMapper.java
@@ -1,7 +1,5 @@
 package ch.eth.ir.indexserver.server.exception;
 
-import java.io.IOException;
-
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
@@ -13,13 +11,13 @@ import org.apache.log4j.Logger;
 import ch.eth.ir.indexserver.index.IndexAPI;
 
 @Provider
-public class IOExceptionMapper implements ExceptionMapper<IOException> {
+public class CustomExceptionMapper implements ExceptionMapper<Exception> {
 	private static Logger log = Logger.getLogger(IndexAPI.class);
 
 	@Context
 	private UriInfo uriInfo;
 
-	public Response toResponse(IOException e) {
+	public Response toResponse(Exception e) {
 		String errorMessage = buildErrorMessage(uriInfo);
 		log.error(errorMessage, e);
 		return Response.serverError().entity("Internal Server Error - Please try again!").build();
@@ -27,7 +25,7 @@ public class IOExceptionMapper implements ExceptionMapper<IOException> {
 
 	private String buildErrorMessage(UriInfo uriInfo) {
 		StringBuilder message = new StringBuilder();
-		message.append("IO Exception:\n");
+		message.append("Exception:\n");
 		if (uriInfo != null) {
 			message.append("URL: ").append(uriInfo.getAbsolutePath()).append("\n");
 			message.append("PARAM: ").append(uriInfo.getQueryParameters().toString()).append("\n");
